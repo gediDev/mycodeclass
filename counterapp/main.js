ad966ce6c292546240264da84953e411e2f67e76
gediDev <152314362+gediDev@users.noreply.github.com>
2023-11-28 08:55:39 -0800

    Add files via upload

---
+++ counterapp/main.js
@@ -0,0 +1,40 @@
+//counter app functionality
+
+/*
+object is a collection of related data.
+properties & function 
+*/
+
+//Array Declaration
+let myCarArr = ["Bmw", "benz", "Toyota", "Chevrolet", "ford"]; //an Array
+
+let okoyeSearch = ["porridge beans", "rice"];
+okoyeSearch[1];
+
+let studentname  = "Joseph"; //a string variable
+
+//developer display method...
+console.log(studentname);
+console.log(myCarArr[0]);
+console.log(myCarArr[1]);
+console.log(myCarArr[2]);
+console.log(myCarArr[3]);
+console.log(myCarArr[4]);
+
+
+
+
+
+//Object Declaration
+// let myObj = {};
+
+
+// console.log(
+//     document.getElementById("counter-box").textContent
+//     );
+
+
+
+
+
+
